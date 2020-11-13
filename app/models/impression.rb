5829530f1b28fe60655f8da69d915c99b3817fb3
nagata <mad.20th@gmail.com>
2020-11-13 12:34:12 +0900

    table紐づけ

--- app/models/impression.rb
+++ app/models/impression.rb
@@ -1,2 +1,13 @@
 class Impression < ApplicationRecord
+  validates :title, presence: true
+  validate :validate_title_not_including_comma
+
+  belongs_to :user
+  belongs_to :category
+
+  private
+  
+  def validate_title_not_including_comma
+    errors.add(:title, 'にカンマを含めることはできません') if title&.include?(',')
+  end
 end
