b9a23c7d8b4de5da227bc6f73902dbedf3697aae
nagata <mad.20th@gmail.com>
2020-11-13 12:38:20 +0900

    user_id,category_id追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_063204) do
+ActiveRecord::Schema.define(version: 2020_11_12_061847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,10 @@ ActiveRecord::Schema.define(version: 2020_11_10_063204) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_impressions_on_category_id"
+    t.index ["user_id"], name: "index_impressions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
